7fc2fcaa7ed318b7a08522aabf4481b04f61eb1e
Allison Tearjen <amtearjen@ucdavis.edu>
2018-09-15 19:36:12 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,68 @@
+
+
+class TicTacToe
+
+  def initialize
+    @board  = Array.new(9," ")
+  end
+
+  def board
+    @board
+  end
+
+  def board= (new_board)
+    @board = new_board
+  end
+
+   # Creates an array with 9 elements filled with " "
+   WIN_COMBINATIONS = [
+   [0,1,2], # Top row
+   [3,4,5],  # Middle row
+   [6,7,8], #bottom row
+   [0,3,6], #first columns
+   [1,4,7], #2nd columns
+   [2,5,8], #3rd columns
+   [0,4,8], #first diagonals
+   [2,4,6] #diagonal 2nd
+   ]
+   def input_to_index(input)
+     index = input.to_i
+     index -= 1
+   end
+
+  def current_player(board)
+    turn_count(board) % 2 == 0 ? "X" : "O"
+  end
+
+  def move(index,token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?
+    !(board[index].nil? || board[index] == " ")
+  end
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board,index) == true
+      player_move(board,index,current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+
+  def turn_count(board)
+    board.count{|token| token == "X" || token == "O"}
+  end
+
+  def display_board
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+end
